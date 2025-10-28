e7eff659ea5e3d812a3764e09ec3b54bf171a8d3
Nemanja Milosavljevic <31475477+Nemanja1208@users.noreply.github.com>
2025-10-28 13:10:02 +0100

    Add project files.

---
+++ Library.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JsonExerciseLists
+{
+    public class Library
+    {
+        [JsonPropertyName("books")]
+        public List<Book> Books { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace JsonExerciseLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // If you want to use only an array of books without a wrapper class:
+            //string filePath = "books.json";
+            //string json = File.ReadAllText(filePath);
+            //var books = JsonSerializer.Deserialize<List<Book>>(json);
+
+            // If you want to use only different data in the same json as books, authors etc...
+            // Then you need a wrapper class like Library to hold the array of books.
+            string filePath = "booksList.json";
+            string json = File.ReadAllText(filePath);
+            var library = JsonSerializer.Deserialize<Library>(json);
+
+            foreach (var book in library.Books)
+            {
+                Console.WriteLine($"{book.Title} by {book.Author} ({book.Year}) - {book.Genre} - ${book.Price}");
+            }
+        }
+    }
+}
